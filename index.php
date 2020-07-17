a1f70917c527c8976b77e0ecddbd2185eaeb3878
practicante2syg <practicante2@syg.com.co>
2020-07-17 17:22:54 -0500

    actualizando formulario

--- index.php
+++ index.php
@@ -21,11 +21,11 @@
 
                <div class="campo">
                     <label for="nombre">Telefono:</label>
-                    <input type="tel" placeholder="Numero de contacto" id="telefono"> 
-               </div>
-               <div class="enviar campo"> 
-                    <input type="submit" value="Añadir">
-               </div>
+                    <input type="tel" placeholder="Numero de contacto" id="nombre"> 
+               </div>               
+          </div>
+          <div class="campo enviar"> 
+               <input type="submit" value="Añadir">
           </div>
      </form>
 </div>
